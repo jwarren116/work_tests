1a56bd083341f3d77400a6ab8860605a66177d07
James Warren <jwarren116@gmail.com>
2015-07-18 09:58:42 -0700

    Compare cameras in camera_list to models in known_cameras to find and map closest match, else map to None

--- mapper.py
+++ mapper.py
@@ -1,21 +1,28 @@
 #!/usr/bin/env python
 import io
-import re
+import difflib
 
 
 camera_map = {}
 
-# read contents of file into camera_list
-camera_list = io.open('camera_list.txt', mode='r').readlines()
+# read contents of file into camera_list and strip line breaks
+cameras = io.open('camera_list.txt', mode='r').readlines()
+cameras = [camera.strip() for camera in cameras]
 
-# strip extra whitespace and line breaks from each camera in list
-cameras = [camera.strip() for camera in camera_list]
+# read in current list of known cameras and strip line breaks
+known_cameras = io.open('known_cameras.txt', mode='r').readlines()
+known_cameras = [camera.strip() for camera in known_cameras]
 
 # converting to set and back to list removes duplicate values
 cameras = list(set(cameras))
 
-# iterate through camera list, map camera to title cased version
+# assign closest match to camera in known_cameras list to value in camera_map
+# if a match is not found, map value to None
 for camera in cameras:
-    camera_map[camera] = camera
+    camera_result = difflib.get_close_matches(camera, known_cameras, n=1, cutoff=0.4)
+    try:
+        camera_map[camera] = camera_result[0]
+    except IndexError:
+        camera_map[camera] = None
 
-print len(camera_map)
+print camera_map
